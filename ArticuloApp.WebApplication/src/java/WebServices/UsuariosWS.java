0cf71912327c4a2fdc07db7fed868fb48d76f887
mfmontess <mmontess@ucentral.edu.co>
2018-04-26 08:42:49 -0400

    Library
    Aplicacion java en la cual se define el modelo del proyecto

---
+++ ArticuloApp.WebApplication/src/java/WebServices/UsuariosWS.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package WebServices;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+
+/**
+ *
+ * @author Administrator
+ */
+@WebService(serviceName = "UsuariosWS")
+public class UsuariosWS {
+
+    /**
+     * This is a sample web service operation
+     */
+    @WebMethod(operationName = "hello")
+    public String hello(@WebParam(name = "name") String txt) {
+        return "Hello " + txt + " !";
+    }
+    
+    /**
+     * This is a sample web service operation
+     * @param usuario
+     * @return 
+     */
+    @WebMethod(operationName = "GetCliente")
+    public String GetCliente(@WebParam(name = "usuario") String usuario) {
+        return null;
+    }
+}
