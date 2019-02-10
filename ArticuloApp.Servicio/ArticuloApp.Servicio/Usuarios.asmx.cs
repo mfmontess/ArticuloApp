8a97f36a7edc7de33ca31a5d4a79872e79560e27
mfmontess <mmontess@ucentral.edu.co>
2019-02-10 16:34:13 -0500

    migraciòn de servicio web a .NET
    creaciòn de servicio web en .NET

---
+++ ArticuloApp.Servicio/ArticuloApp.Servicio/Usuarios.asmx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace ArticuloApp.Servicio
+{
+    /// <summary>
+    /// Descripción breve de Usuarios
+    /// </summary>
+    [WebService(Namespace = "http://articuloApp.com/Usuarios/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class Usuarios : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hola a todos";
+        }
+
+        [WebMethod]
+        public string Registrar()
+        {
+            return "Hola a todos";
+        }
+
+        [WebMethod]
+        public string Autenticar(string prmstrUsuario, string prmstrClave)
+        {
+            return $"Felicidades se acaba de autenticar el usuario : {prmstrUsuario}";
+        }
+    }
+}
